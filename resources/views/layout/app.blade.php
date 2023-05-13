04348627ec723375d95c8ff37b9af8e5ea962821
Rayandra Wandi Marselana <mwandirayandra@gmail.com>
2023-05-13 14:32:10 +0700

    DONE : Dashboard

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,29 @@
+@include('layout.partials.header')
+
+<body class="nav-md">
+    <div class="container body">
+        <div class="main_container">
+            <div class="col-md-3 left_col">
+                <div class="left_col scroll-view">
+
+                    @include('layout.partials.profil_info')
+
+                    @include('layout.partials.side_bar')
+
+                    @include('layout.partials.side_bar_footer')
+                </div>
+            </div>
+
+            @include('layout.partials.navbar')
+
+            @yield('content')
+
+            @include('layout.partials.info_footer')
+        </div>
+    </div>
+
+    @include('layout.partials.footer')
+
+</body>
+
+</html>
---
+++ resources/views/layout/partials/header.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="images/favicon.ico" type="image/ico" />
+
+    <title>{{ config('app.name') }}</title>
+
+    <!-- Bootstrap -->
+    <link href="{{ template_gentelellaMaster() }}/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="{{ template_gentelellaMaster() }}/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <!-- NProgress -->
+    <link href="{{ template_gentelellaMaster() }}/vendors/nprogress/nprogress.css" rel="stylesheet">
+    <!-- iCheck -->
+    <link href="{{ template_gentelellaMaster() }}/vendors/iCheck/skins/flat/green.css" rel="stylesheet">
+
+    <!-- bootstrap-progressbar -->
+    <link href="{{ template_gentelellaMaster() }}/vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css"
+        rel="stylesheet">
+    <!-- JQVMap -->
+    <link href="{{ template_gentelellaMaster() }}/vendors/jqvmap/dist/jqvmap.min.css" rel="stylesheet" />
+    <!-- bootstrap-daterangepicker -->
+    <link href="{{ template_gentelellaMaster() }}/vendors/bootstrap-daterangepicker/daterangepicker.css"
+        rel="stylesheet">
+
+    <!-- Custom Theme Style -->
+    <link href="{{ template_gentelellaMaster() }}/build/css/custom.min.css" rel="stylesheet">
+</head>
